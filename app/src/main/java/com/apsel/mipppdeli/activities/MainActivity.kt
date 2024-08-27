bfaaf4be8c51763230ad599ea046f9d6b8a0a345
natless77 <lastnagusarname@gmail.com>
2024-08-27 10:27:44 -0500

    ddd

--- app/src/main/java/com/apsel/mipppdeli/activities/MainActivity.kt
+++ app/src/main/java/com/apsel/mipppdeli/activities/MainActivity.kt
@@ -2,7 +2,10 @@ package com.apsel.mipppdeli.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,14 +15,28 @@ import com.apsel.mipppdeli.R
 class MainActivity : AppCompatActivity() {
 
     var imageViewGoToRegister: ImageView? = null
+    var editTextEmail: EditText? = null
+    var editTextPassword: EditText? = null
+    var buttonLogin: Button? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         imageViewGoToRegister = findViewById(R.id.imageview_go_to_register)
+        editTextEmail = findViewById(R.id.edittext_email)
+        editTextPassword = findViewById(R.id.edittext_password)
+        buttonLogin = findViewById(R.id.btn_login)
 
         imageViewGoToRegister?.setOnClickListener { goToRegister() }
+    }
+
+    private fun login(){
+        val email = editTextEmail?.text.toString()
+        val password = editTextPassword?.text.toString()
+
+        Toast.makeText(this, "El email es: ${email}", Toast.LENGTH_SHORT).show()
     }
 
     private fun goToRegister(){
