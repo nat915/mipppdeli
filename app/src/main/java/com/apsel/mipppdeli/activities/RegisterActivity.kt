235ca14bb2dc9b2fe3bf6e70a5f05f42424ccbaf
natless77 <lastnagusarname@gmail.com>
2024-08-26 22:59:37 -0500

    hola123

--- app/src/main/java/com/apsel/mipppdeli/activities/RegisterActivity.kt
+++ app/src/main/java/com/apsel/mipppdeli/activities/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.apsel.mipppdeli.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -17,7 +18,13 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        imageViewGoToLogin = findViewById(R.id.imageview_go_to_login)
 
+        imageViewGoToLogin?.setOnClickListener { goToLogin() }
+    }
 
+    private fun goToLogin(){
+        val i = Intent(this, MainActivity::class.java)
+        startActivity(i)
     }
 }
