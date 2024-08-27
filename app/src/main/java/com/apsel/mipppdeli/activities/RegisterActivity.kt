85d651d0071e51b995ab5e1351de4ad7c3411c8b
natless77 <lastnagusarname@gmail.com>
2024-08-26 22:51:56 -0500

    hola

--- app/src/main/java/com/apsel/mipppdeli/activities/RegisterActivity.kt
+++ app/src/main/java/com/apsel/mipppdeli/activities/RegisterActivity.kt
@@ -1,13 +1,23 @@
 package com.apsel.mipppdeli.activities
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.apsel.mipppdeli.R
 
+
+
+
+
 class RegisterActivity : AppCompatActivity() {
+
+    var imageViewGoToLogin: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
 
     }
 }
