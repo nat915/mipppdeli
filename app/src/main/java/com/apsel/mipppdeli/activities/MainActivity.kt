c2178102db1242e2efd32644c643ad3c64440c1a
natless77 <lastnagusarname@gmail.com>
2024-08-26 19:08:28 -0500

    nuevaa

--- app/src/main/java/com/apsel/mipppdeli/activities/MainActivity.kt
+++ app/src/main/java/com/apsel/mipppdeli/activities/MainActivity.kt
@@ -1,20 +1,21 @@
-package com.apsel.mipppdeli
+package com.apsel.mipppdeli.activities
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.apsel.mipppdeli.R
 
 class MainActivity : AppCompatActivity() {
+
+    var imageViewGoToRegister: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        imageViewGoToRegister = findViewById(R.id.imageview_go_to_register)
     }
 }
